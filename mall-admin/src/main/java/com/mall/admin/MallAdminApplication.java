fe056adeaf3765e1c8fc6d9c016be16643e2b6f3
Wangyufu19 <youfu.wang@gmail.com>
2021-11-03 15:15:28 +0800

    提交jbos-mall模块功能

--- mall-admin/src/main/java/com/mall/admin/MallAdminApplication.java
+++ mall-admin/src/main/java/com/mall/admin/MallAdminApplication.java
@@ -1,17 +1,11 @@
 package com.mall.admin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * AdminApplication
