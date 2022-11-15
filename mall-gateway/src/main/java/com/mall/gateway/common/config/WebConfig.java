2da7bc3720d7f753afbd828c5b91c3da485999a7
Wangyufu19 <youfu.wang@gmail.com>
2022-11-15 10:22:18 +0800

    i

---
+++ mall-gateway/src/main/java/com/mall/gateway/common/config/WebConfig.java
@@ -0,0 +1,61 @@
+package com.mall.gateway.common.config;
+
+import com.mall.common.response.ResponseData;
+import com.mall.common.utils.JacksonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${zuul.filter.excludeUri}")
+    private String excludeUri;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowCredentials(true)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .maxAge(3600);
+    }
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TokenInterceptor())
+                .excludePathPatterns(excludeUri.split(","))
+                .addPathPatterns("/**");
+    }
+    public class TokenInterceptor implements HandlerInterceptor {
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            log.info("uri={}",request.getRequestURI());
+            String accessToken = getRequestToken(request);
+            if(accessToken == null) {
+                ResponseData r = ResponseData.error("token失效或认证过期！");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setContentType("application/json;charset=utf-8");
+                PrintWriter out = response.getWriter();
+                out.write(JacksonUtils.toJson(r));
+                out.flush();
+                out.close();
+                return false;
+            }
+            return true;
+        }
+        private String getRequestToken(HttpServletRequest request) {
+            String accessToken = request.getHeader("accessToken");
+            if (accessToken == null) {
+                return request.getParameter("accessToken");
+            } else {
+                return accessToken;
+            }
+        }
+    }
+}
