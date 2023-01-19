d09d3c9d3b858185f94d829f3c3c83dde6d39715
wangyufu19 <youfu.wang@gmail.com>
2023-01-19 10:43:59 +0800

    提交jbos-mall模块功能

--- mall-gateway/src/main/java/com/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/mall/gateway/MallGatewayApplication.java
@@ -2,15 +2,20 @@ package com.mall.gateway;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.mall.gateway.common.spring.ApplicationContextListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -22,6 +27,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class MallGatewayApplication extends SpringBootServletInitializer {
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -34,5 +40,18 @@ public class MallGatewayApplication extends SpringBootServletInitializer {
         SpringApplication springApplication=new SpringApplication(MallGatewayApplication.class);
         springApplication.addListeners(new ApplicationContextListener());
         ApplicationContext applicationContext=springApplication.run(args);
+        RestDemo restDemo=applicationContext.getBean(RestDemo.class);
+        restDemo.execute();
+    }
+    @Slf4j
+    @Component
+    public static class RestDemo{
+        @Autowired
+        RestTemplate restTemplate;
+
+        public void execute(){
+            ResponseEntity<String> responseEntity=restTemplate.getForEntity("http://mall-admin/user/getUserInfo",String.class);
+           log.info("res={}",responseEntity.getBody());
+        }
     }
 }
