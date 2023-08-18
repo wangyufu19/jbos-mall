e5e83b62958e30538f9ab3341e7e824b1469a1d3
wangy <youfu.wang@gmail.com>
2023-08-18 17:12:05 +0800

    commit jbos mall-admin

--- mall-admin/src/main/java/com/mall/admin/application/api/comm/FileUploadApi.java
+++ mall-admin/src/main/java/com/mall/admin/application/api/comm/FileUploadApi.java
@@ -4,13 +4,18 @@ import com.mall.common.response.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
 /**
  * FileUploadApi
+ *
  * @author youfu.wang
  * @date 2023/4/13
  **/
@@ -21,18 +26,20 @@ import java.util.Map;
 public class FileUploadApi {
     /**
      * 上传一个文件
+     * @param params
      * @param file
-     * @return
+     * @return ResponseResult
      */
     @ResponseBody
     @PostMapping(value = "/upload")
     @ApiOperation("上传一个文件")
-    public ResponseResult upload(MultipartFile file, @RequestParam Map<String, Object> params){
-        ResponseResult res= ResponseResult.ok();
-        try{
-        }catch (Exception e){
-            log.error(e.getMessage(),e);
-            res= ResponseResult.error(ResponseResult.CODE_FAILURE, ResponseResult.MSG_FAILURE);
+    public ResponseResult upload(MultipartFile file, @RequestParam Map<String, Object> params) {
+        ResponseResult res = ResponseResult.ok();
+        try {
+            log.info("upload");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            res = ResponseResult.error(ResponseResult.CODE_FAILURE, ResponseResult.MSG_FAILURE);
         }
         return res;
     }
